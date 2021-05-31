cbefc043cd2a8b6e7405dfae80aff4cfe1501c6e
vincenthanguk <vincentkreusel@gmail.com>
2021-05-31 22:16:53 +0200

    solved bonus exercise,need to cleanup raw data...

--- data.js
+++ data.js
@@ -1,64 +1,4 @@
-'use strict';
-// 1) create nested array out of map string
-
-const mapString =
-  '..##.......,#...#...#..,.#....#..#.,..#.#...#.#,.#...##..#.,..#.##.....,.#.#.#....#,.#........#,#.##...#...,#...##....#,.#..#...#.#'.split(
-    ','
-  );
-
-const mapArr = [];
-for (let i = 0; i < mapString.length; i++) {
-  mapArr[i] = mapString[i].split('');
-}
-
-// 2) function for traversing the map with slope right 3, down 1. function takes the nested map array and starting index
-
-// const countTrees = function (arr, startIndex) {
-//   // modulo 11 for index>11
-//   //   0, 1, 2, ..., 10, 0, 1, 2,...
-
-//   let index = startIndex % 11;
-//   let treeArray = [];
-
-//   for (let row = 0; row < arr.length; row++) {
-//     //   only add 1 in first map[0]
-//     if (row === 0) row++;
-//     // increment index by 3
-//     index = index + 3;
-//     if (index > 10) index = index - arr[row].length;
-//     // push 'O' for '.', 'X' for '#'
-//     arr[row][index] === '.' ? treeArray.push('O') : treeArray.push('X');
-//   }
-//   return treeArray;
-// };
-
-const countTrees = function (arr, startIndex) {
-  // modulo 11 for index>11
-  //   0, 1, 2, ..., 10, 0, 1, 2,...
-
-  let index = startIndex % arr[0].length;
-  let trees = 0;
-
-  for (let row = 0; row < arr.length; row++) {
-    //   only add 1 in first map[0]
-    if (row === 0) row++;
-    // increment index by 3
-    index = index + 3;
-    if (index >= arr[row].length) index = index - arr[row].length;
-    // push 'O' for '.', 'X' for '#'
-    if (arr[row][index] === '#') trees++;
-  }
-  return trees;
-};
-
-console.log(mapArr[0].length);
-
-console.log(countTrees(mapArr, 0));
-// 3) returns ["O", "X", "O", "X", "X", "O", "X", "X", "X", "X"]
-console.log(countTrees(mapArr, 11)); // returns same as above
-
-const map = `
-.#...#.......#...#...#.#.#.....
+const map = `.#...#.......#...#...#.#.#.....
 ####.....#.#..#...#...........#
 .....#...........#......#....#.
 ......#..#......#.#..#...##.#.#
@@ -383,10 +323,4 @@ const map = `
 .....#.......#######.#.#...#...`;
 
 const largeMap = map.replace(/(\r\n|\n|\r)/gm, ',');
-
-const largeMapArr = largeMap.slice(1).split(',');
-for (let i = 0; i < mapString.length; i++) {
-  mapArr[i] = mapString[i].split('');
-}
-
-console.log(countTrees(largeMapArr, 0));
+export { largeMap };
