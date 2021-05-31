30619b0634564482dc62aed251127ba69b76c866
vincenthanguk <vincentkreusel@gmail.com>
2021-05-31 20:45:05 +0200

    solved?

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-"use strict";
+'use strict';
+
+const mapString =
+  '..##.......,#...#...#..,.#....#..#.,..#.#...#.#,.#...##..#.,..#.##.....,.#.#.#....#,.#........#,#.##...#...,#...##....#,.#..#...#.#'.split(
+    ','
+  );
+
+const mapArr = [];
+for (let i = 0; i < mapString.length; i++) {
+  mapArr[i] = mapString[i].split('');
+}
+
+// const map = [
+//   '..##.......'.split(''),
+//   '#...#...#..'.split(''),
+//   '.#....#..#.'.split(''),
+//   '..#.#...#.#'.split(''),
+//   '.#...##..#.'.split(''),
+//   '..#.##.....'.split(''),
+//   '.#.#.#....#'.split(''),
+//   '.#........#'.split(''),
+//   '#.##...#...'.split(''),
+//   '#...##....#'.split(''),
+//   '.#..#...#.#'.split(''),
+// ];
+
+const countTrees = function (arr, startRow, startIndex) {
+  let index = startIndex;
+  let treeArray = [];
+
+  for (let row = startRow; row < arr.length; row++) {
+    if (row === 0) row++;
+    index = index + 3;
+    if (index > 10) index = index - arr[row].length;
+    arr[row][index] === '.' ? treeArray.push('O') : treeArray.push('X');
+  }
+  return treeArray;
+};
+
+console.log(countTrees(mapArr, 0, 0));
